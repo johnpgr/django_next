f4f354c894a86cbadba38076acb6117016ead226
johnpgr <jp.greidinger@hotmail.com>
2025-05-22 13:29:06 -0300

    Fix server dockerfile

--- server/config/settings.py
+++ server/config/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Only load .env if not in production
 if not os.environ.get('DJANGO_SETTINGS_PRODUCTION', '').lower() == 'true':
     from dotenv import load_dotenv
-    load_dotenv(os.path.join(BASE_DIR, '.env'))
+    load_dotenv(os.path.join(BASE_DIR, '.env.development'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
